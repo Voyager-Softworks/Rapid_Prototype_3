a0f9c6c91de3c899a9f457d938a5d44b0e497e16
Keane <KeaneCarotenuto@gmail.com>
2021-10-19 22:21:58 +1300

    Jerry Cans

---
+++ Assets/Jerrycan.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jerrycan : MonoBehaviour
+{
+    public bool m_held = false;
+    public bool m_placed = false;
+    public bool m_blown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
