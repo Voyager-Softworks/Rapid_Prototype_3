e8be0d499732eaea12f660d08048c46fbfea8714
Keane <KeaneCarotenuto@gmail.com>
2021-10-24 01:44:28 +1300

    Added the easter egg

--- Assets/GenericEvent.cs
+++ Assets/GenericEvent.cs
@@ -6,8 +6,25 @@ using UnityEngine.Events;
 public class GenericEvent : MonoBehaviour
 {
     public UnityEvent OnEvent;
+    public bool RunOnAwake = false;
+    public float Delay = 0.0f;
+    bool triggering = false;
     public void TriggerEvent()
     {
         OnEvent.Invoke();
+    }
+
+    void Awake()
+    {
+        if (RunOnAwake) triggering = true;
+    }
+
+    void Update()
+    {
+        if (triggering && (Delay -= Time.deltaTime) < 0.0f)
+        {
+            triggering = false;
+            OnEvent.Invoke();
+        }
     }
 }
